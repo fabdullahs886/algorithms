a386833a6bafe553c7198201e623d88f6e1eecae
faisabdu <faisal.abdullah@here.com>
2020-08-11 18:23:25 -0500

    2 solutions implemented

--- leetcode/java/src/com/problems/easy/LongestCommonPrefix.java
+++ leetcode/java/src/com/problems/easy/LongestCommonPrefix.java
@@ -1,3 +1,6 @@
+package com.problems.easy;
+
+// Category : Easy
 //Write a function to find the longest common prefix string amongst an array of strings.
 //
 //        If there is no common prefix, return an empty string "".
@@ -11,17 +14,34 @@
 //        Input: ["dog","racecar","car"]
 //        Output: ""
 //        Explanation: There is no common prefix among the input strings.
-//TODO: check other solution
-//https://leetcode.com/explore/learn/card/array-and-string/203/introduction-to-string/1162/
-
-package com.strings;
 
 public class LongestCommonPrefix {
 
     private String longestCommonPrefix(String[] strs) {
+        if( strs == null || strs.length == 0) return "";
+        if(strs.length == 1) return strs[0];
+
+        String result = solution1(strs);
+        result = solution2(strs);
+        return result;
+    }
+
+    private String solution2(String[] strs){
         if( strs.length <= 0) return "";
-        System.out.println("flower".indexOf("flower"));
-        System.out.println("flow".indexOf("flower"));
+
+        for(int i = 0; i < strs[0].length(); i++){
+            char c = strs[0].charAt(i);
+            for(int j = 0; j < strs.length; j++ ){
+                if( i == strs[j].length() || strs[j].charAt(i) != c ){
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+    private String solution1(String[] strs){
+        if( strs.length <= 0) return "";
 
         String prefix = strs[0];
         for(int i = 0; i < strs.length; i ++){
@@ -32,10 +52,15 @@ public class LongestCommonPrefix {
 
         }
         return prefix;
+
     }
 
     public static void main(String[] args){
         LongestCommonPrefix sol = new LongestCommonPrefix();
+        System.out.println(sol.longestCommonPrefix(new String[]{"aa","a"})); // ans: a
+        System.out.println(sol.longestCommonPrefix(new String[]{"c","c"})); // ans: c
+        System.out.println(sol.longestCommonPrefix(new String[]{})); // ans: ""
+        System.out.println(sol.longestCommonPrefix(new String[]{"a"})); // ans: fl
         System.out.println(sol.longestCommonPrefix(new String[]{"flower","flow","flight"})); // ans: fl
         System.out.println(sol.longestCommonPrefix(new String[]{"dog","racecar","car"})); // ans: ""
 
