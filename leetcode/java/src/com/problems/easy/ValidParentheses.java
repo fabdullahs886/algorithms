57728668bb1b04930f7bf3df80851391bba64dae
faisabdu <faisal.abdullah@here.com>
2020-08-11 20:26:59 -0500

    valid parentheses solution implemented

---
+++ leetcode/java/src/com/problems/easy/ValidParentheses.java
@@ -0,0 +1,72 @@
+package com.problems.easy;
+
+//Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+//        An input string is valid if:
+//
+//        Open brackets must be closed by the same type of brackets.
+//        Open brackets must be closed in the correct order.
+//        Note that an empty string is also considered valid.
+//
+//        Example 1:
+//
+//        Input: "()"
+//        Output: true
+//        Example 2:
+//
+//        Input: "()[]{}"
+//        Output: true
+//        Example 3:
+//
+//        Input: "(]"
+//        Output: false
+//        Example 4:
+//
+//        Input: "([)]"
+//        Output: false
+//        Example 5:
+//
+//        Input: "{[]}"
+//        Output: true
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    private boolean isValid(String s) {
+        if( s == null) return false;
+        if(s.trim().length() == 0) return true;
+
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if (c == '(' || c =='{' || c == '[' ) {
+                stack.push(c);
+            }else {
+                if(stack.isEmpty()) return false;
+                char pop = stack.pop();
+                if( (c == ')' && pop != '(') || (c == '}' && pop != '{') || (c == ']' && pop != '[')){
+                    return false;
+                }
+            }
+        }
+        if(!stack.isEmpty())
+            return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidParentheses obj = new ValidParentheses();
+        System.out.println(obj.isValid("")); //ans: true
+        System.out.println(obj.isValid("[(){}]")); //ans: true
+        System.out.println(obj.isValid("[({})]")); //ans: true
+        System.out.println(obj.isValid("]]")); //ans: false
+        System.out.println(obj.isValid("]")); //ans: false
+        System.out.println(obj.isValid("((")); //ans: false
+        System.out.println(obj.isValid("[")); //ans: false
+        System.out.println(obj.isValid("()")); //ans: true
+        System.out.println(obj.isValid("()[]{}")); //ans: true
+        System.out.println(obj.isValid("(]")); //ans: false
+        System.out.println(obj.isValid("([)]")); //ans: false
+        System.out.println(obj.isValid("{[]}")); //ans: true
+    }
+}
